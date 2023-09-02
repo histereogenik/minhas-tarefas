f889282b696e9a32716d8fd101fb295d039dee3c
Humberto Nacif <gigantedeluz@gmail.com>
2023-09-01 21:45:03 -0300

    criando página de cadastro e estilizando

---
+++ src/containers/Formulario/index.tsx
@@ -0,0 +1,24 @@
+import { BotaoSalvar, Campo, MainContainer, Titulo } from '../../styles'
+import { Form, Opcoes } from './styles'
+
+const Formulario = () => (
+  <MainContainer>
+    <Titulo>Nova Tarefa</Titulo>
+    <Form>
+      <Campo type="text" placeholder="Título" />
+      <Campo as="textarea" placeholder="Descrição da tarefa" />
+      <Opcoes>
+        <p>Prioridade</p>
+        <input name="prioridade" type="radio" id="urgente" />
+        <label htmlFor="urgente">Urgente</label>
+        <input name="prioridade" type="radio" id="importante" />
+        <label htmlFor="importante">Importante</label>
+        <input name="prioridade" type="radio" id="normal" />
+        <label htmlFor="normal">Normal</label>
+      </Opcoes>
+      <BotaoSalvar type="submit">Cadastrar</BotaoSalvar>
+    </Form>
+  </MainContainer>
+)
+
+export default Formulario
