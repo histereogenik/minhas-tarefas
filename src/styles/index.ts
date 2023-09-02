8039f13f3ddf9c9c100ae0a8928039851702fbf6
Humberto Nacif <gigantedeluz@gmail.com>
2023-09-01 22:56:03 -0300

    finalizando o botao de voltar a lista de tarefas

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,5 @@
 import { createGlobalStyle, styled } from 'styled-components'
 import variaveis from './variaveis'
-import { Botao } from '../components/Tarefa/styles'
 
 const EstiloGlobal = createGlobalStyle`
   * {
@@ -36,8 +35,18 @@ export const Campo = styled.input`
   border-color: #666666;
   width: 100%;
 `
+export const Botao = styled.button`
+  font-size: 12px;
+  color: #fff;
+  font-weight: bold;
+  padding: 8px 12px;
+  border: none;
+  cursor: pointer;
+  background-color: #2f3640;
+  border-radius: 8px;
+  margin-right: 8px;
+`
 export const BotaoSalvar = styled(Botao)`
   background-color: ${variaveis.verde};
 `
-
 export default EstiloGlobal
